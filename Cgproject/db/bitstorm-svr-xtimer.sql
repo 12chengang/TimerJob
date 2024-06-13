52593f62e0560d53f878c3b2bdda6c8c038cce4d
chengang12 <12142851+chengan12@user.noreply.gitee.com>
2024-06-13 21:28:51 +0800

    数据库表

--- Cgproject/db/bitstorm-svr-xtimer.sql
+++ Cgproject/db/bitstorm-svr-xtimer.sql
@@ -1,24 +1,3 @@
-/*
-SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 8.0.25 : Database - mall4cloud_order
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`bitstorm-svr` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 */;
-
-USE `bitstorm-svr`;
-
-/*Table structure for table `xtimer` */
-
-DROP TABLE IF EXISTS `xtimer`;
 
 CREATE TABLE `xtimer` (
                            `timer_id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'TimerId',
@@ -32,8 +11,6 @@ CREATE TABLE `xtimer` (
                            PRIMARY KEY (`timer_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Timer 信息';
 
-
-/*Table structure for table `timer_task` */
 DROP TABLE IF EXISTS `timer_task`;
 
 CREATE TABLE `timer_task` (
@@ -50,7 +27,3 @@ CREATE TABLE `timer_task` (
                          UNIQUE KEY `idx_timer_id_run_timer` (`timer_id`,`run_timer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Timer Task任务信息';
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
